f0864d1cab2bddf5f55bac760378051eccdc0d40
Nuno Ferreira <nunoferreira840@gmail.com>
2022-01-07 15:57:45 +0000

    OpenAPI customized

--- src/main/java/pt/up/fe/cosn/gateway/GatewayApplication.java
+++ src/main/java/pt/up/fe/cosn/gateway/GatewayApplication.java
@@ -1,15 +1,30 @@
 package pt.up.fe.cosn.gateway;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("COSN API GATEWAY").version("1.0.0"))
+                // Components section defines Security Scheme "mySecretHeader"
+                .addSecurityItem(new SecurityRequirement().addList("my security"))
+                .components(new Components().addSecuritySchemes("my security",
+                        new SecurityScheme().name("mysecurity").type(SecurityScheme.Type.HTTP).scheme("basic")));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
-
 
 }
