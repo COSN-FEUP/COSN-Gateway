35245bf4bd4c36cf64825b2cc3e65db262770a14
Joao Ferreira <joao.n.ferreira@aitec.pt>
2022-01-07 16:49:35 +0000

    custom OpenApi description

--- src/main/java/pt/up/fe/cosn/gateway/GatewayApplication.java
+++ src/main/java/pt/up/fe/cosn/gateway/GatewayApplication.java
@@ -1,10 +1,24 @@
 package pt.up.fe.cosn.gateway;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().info(new Info().title("COSN API Gateway").version("1.0.0"));
+                /*.addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .components(new Components().addSecuritySchemes("Authorization", new SecurityScheme().name("Authorization").type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer").bearerFormat("JWT")));*/
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
