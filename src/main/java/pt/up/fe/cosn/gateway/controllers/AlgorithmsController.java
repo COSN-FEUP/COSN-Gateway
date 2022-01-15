68029cc173c0b13825487d05e6e6e6e8acbb5feb
Nuno Ferreira <nunoferreira840@gmail.com>
2022-01-15 16:14:22 +0000

    initial development of routing integration

--- src/main/java/pt/up/fe/cosn/gateway/controllers/AlgorithmsController.java
+++ src/main/java/pt/up/fe/cosn/gateway/controllers/AlgorithmsController.java
@@ -1,10 +1,14 @@
 package pt.up.fe.cosn.gateway.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import pt.up.fe.cosn.gateway.advices.responses.algorithmsResponses.AlgorithmResponse;
 import pt.up.fe.cosn.gateway.advices.responses.SimpleResponse;
+import pt.up.fe.cosn.gateway.advices.responses.algorithmsResponses.WorkflowResponse;
 import pt.up.fe.cosn.gateway.entities.Algorithm;
 import pt.up.fe.cosn.gateway.entities.User;
 import pt.up.fe.cosn.gateway.factories.ResponseFactory;
@@ -12,9 +16,8 @@ import pt.up.fe.cosn.gateway.services.RoleService;
 import pt.up.fe.cosn.gateway.services.UserService;
 import pt.up.fe.cosn.gateway.utils.Utils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 public class AlgorithmsController {
@@ -25,12 +28,20 @@ public class AlgorithmsController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     private final String[] mockAlgorithmsList = {"K-means", "DBSCAN", "Neural Network", "LightGBM", "Decision Tree"};
     private final List<Algorithm> Algorithms = new ArrayList<>();
+
+    private static String algorithmURL = "http://algorithmmicroservice.herokuapp.com";
 
     @GetMapping("/getAlgorithms")
     @ResponseBody
     public ResponseEntity<Object> getAlgorithms(@RequestHeader("AuthToken") String authorization) {
+        String getAlgorithmsUrlPath = algorithmURL+"/algorithms";
+        String tempURL = "https://61d8d203e6744d0017ba8cc5.mockapi.io/Algorithms";
+
         Claims claim = Utils.decodeJWT(authorization);
         Optional<User> userOptional = userService.getUserByEmail(claim.getSubject());
 
@@ -40,11 +51,94 @@ public class AlgorithmsController {
         if(userOptional.get().getRole().getValue() > roleService.getAdministratorRole().get().getValue())
             return ResponseFactory.unauthorized(new SimpleResponse(false, "User is not authorized to do this operation."));
 
-        //TODO Temporary mock values
-        for (String mockAlgorithm : mockAlgorithmsList) {
-            Algorithms.add(new Algorithm(mockAlgorithm));
-        }
+        ResponseEntity<AlgorithmResponse[]> response =
+                restTemplate.getForEntity(
+                        tempURL,
+                        AlgorithmResponse[].class);
+        AlgorithmResponse[] algorithms = response.getBody();
 
-        return ResponseFactory.ok(Algorithms);
+        return ResponseFactory.ok(algorithms);
     }
+
+    @GetMapping("/getAlgorithmsName")
+    @ResponseBody
+    public ResponseEntity<Object> getAlgorithmsNames(@RequestHeader("AuthToken") String authorization) {
+        String getAlgorithmsUrlPath = algorithmURL+"/algorithms";
+        String tempURL = "https://61d8d203e6744d0017ba8cc5.mockapi.io/Algorithms";
+
+        Claims claim = Utils.decodeJWT(authorization);
+        Optional<User> userOptional = userService.getUserByEmail(claim.getSubject());
+
+        if(userOptional.isEmpty())
+            return ResponseFactory.bad(new SimpleResponse(false, "The token is not valid."));
+
+        if(userOptional.get().getRole().getValue() > roleService.getAdministratorRole().get().getValue())
+            return ResponseFactory.unauthorized(new SimpleResponse(false, "User is not authorized to do this operation."));
+
+        ResponseEntity<AlgorithmResponse[]> response =
+                restTemplate.getForEntity(
+                        tempURL,
+                        AlgorithmResponse[].class);
+        AlgorithmResponse[] algorithms = response.getBody();
+
+        //Return only the map or anything else
+        ObjectMapper mapper = new ObjectMapper();
+
+        return ResponseFactory.ok(Arrays.stream(algorithms)
+                .map(object -> mapper.convertValue(object, AlgorithmResponse.class))
+                .map(AlgorithmResponse::getName)
+                .collect(Collectors.toList()));
+    }
+
+    @GetMapping("/getAlgorithmById")
+    @ResponseBody
+    public ResponseEntity<Object> getAlgorithmById(@RequestHeader("AuthToken") String authorization, @RequestHeader("Id") String id) {
+        String getAlgorithmsUrlPath = algorithmURL+"/algorithms/"+id;
+        String tempURL = "https://61d8d203e6744d0017ba8cc5.mockapi.io/Algorithms/"+id;
+
+        Claims claim = Utils.decodeJWT(authorization);
+        Optional<User> userOptional = userService.getUserByEmail(claim.getSubject());
+
+        if(userOptional.isEmpty())
+            return ResponseFactory.bad(new SimpleResponse(false, "The token is not valid."));
+
+        if(userOptional.get().getRole().getValue() > roleService.getAdministratorRole().get().getValue())
+            return ResponseFactory.unauthorized(new SimpleResponse(false, "User is not authorized to do this operation."));
+
+        ResponseEntity<AlgorithmResponse> response =
+                restTemplate.getForEntity(
+                        tempURL,
+                        AlgorithmResponse.class);
+
+        AlgorithmResponse algorithm = response.getBody();
+        //Get response status code
+        int statusCodeValue = response.getStatusCodeValue();
+
+        return ResponseFactory.ok(algorithm);
+    }
+
+    @GetMapping("/getWorkflows")
+    @ResponseBody
+    public ResponseEntity<Object> getWorkflows(@RequestHeader("AuthToken") String authorization) {
+        String getAlgorithmsUrlPath = algorithmURL+"/workflows/";
+        String tempURL = "https://61d8d203e6744d0017ba8cc5.mockapi.io/Workflows";
+
+        Claims claim = Utils.decodeJWT(authorization);
+        Optional<User> userOptional = userService.getUserByEmail(claim.getSubject());
+
+        if(userOptional.isEmpty())
+            return ResponseFactory.bad(new SimpleResponse(false, "The token is not valid."));
+
+        if(userOptional.get().getRole().getValue() > roleService.getAdministratorRole().get().getValue())
+            return ResponseFactory.unauthorized(new SimpleResponse(false, "User is not authorized to do this operation."));
+
+        ResponseEntity<WorkflowResponse[]> response =
+                restTemplate.getForEntity(
+                        tempURL,
+                        WorkflowResponse[].class);
+        WorkflowResponse[] workflows = response.getBody();
+
+        return ResponseFactory.ok(workflows);
+    }
+
 }
